2fa77850cf21369fbc66ccab779537eef421ce0e
SirTopHat4000 <rileymckeon@seattleacademy.org>
2025-09-25 16:10:06 -0700

    regester Dcmotors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simpleOpMode.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class simpleOpMode extends LinearOpMode {
+
+    DcMotor leftFrontDrive;
+    DcMotor leftBackDrive;
+    DcMotor rightFrontDrive;
+    DcMotor rightBackDrive;
+
+
+@Override
+public void runOpMode() {
+
+
+    leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFrontDrive");
+
+    leftBackDrive = hardwareMap.get(DcMotor.class, "leftBackDrive");
+
+    rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
+
+    rightBackDrive = hardwareMap.get(DcMotor.class, "rightBackDrive");
+}
+
+}
