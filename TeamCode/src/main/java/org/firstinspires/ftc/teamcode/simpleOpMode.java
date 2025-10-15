eadcd32048a145e278a87a8baf5c098130f0716b
Parzivalrp2 <amithendrickson@seattleacademy.org>
2025-10-14 17:00:08 -0700

    fixed steering and added thrower

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simpleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simpleOpMode.java
@@ -3,60 +3,73 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 @TeleOp(name = "hunter2")
 public class simpleOpMode extends LinearOpMode {
 
     DcMotor leftFrontDrive;
-//    DcMotor leftBackDrive;
     DcMotor rightFrontDrive;
-//    DcMotor rightBackDrive;
+    DcMotor thrower;
+    Servo leftStopper;
+    Servo rightStopper;
 
 
-@Override
-public void runOpMode() {
+    @Override
+    public void runOpMode() {
 
-    telemetry.addData("Status:", "Initializing");
-    telemetry.update();
-
-    leftFrontDrive = hardwareMap.get(DcMotor.class, "FL");
-
-//    leftBackDrive = hardwareMap.get(DcMotor.class, "BL");
-
-    rightFrontDrive = hardwareMap.get(DcMotor.class, "FR");
-
-//    rightBackDrive = hardwareMap.get(DcMotor.class, "BR");
-    telemetry.addData("Status:", "Initialized");
-    telemetry.update();
-waitForStart();
-
-while (opModeIsActive()) {
-
-    float speed;
-    if (gamepad1.right_trigger > 0) {
-        speed = gamepad1.right_trigger;
-        telemetry.addData("Speed:", speed);
+        telemetry.addData("Status:", "Initializing");
         telemetry.update();
-        leftFrontDrive.setPower(speed);
-//     leftBackDrive.setPower(speed);
-        rightFrontDrive.setPower(speed);
-//     rightBackDrive.setPower(speed);
 
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "FL");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "FR");
+        thrower = hardwareMap.get(DcMotor.class, "T");
+//        leftStopper = hardwareMap.get(Servo.class, "LS");
+//        rightStopper = hardwareMap.get(Servo.class, "RS");
+        telemetry.addData("Status:", "Initialized");
+        telemetry.update();
+        waitForStart();
+        float throwerSpeed = 0;
+        float rightSpeed = 0;
+        float leftSpeed = 0;
+        while (opModeIsActive()) {
+
+            if ((gamepad1.right_trigger > 0) || (gamepad1.left_trigger >0)) {
+                rightSpeed = gamepad1.right_trigger;
+                leftSpeed = gamepad1.left_trigger;
+                telemetry.addData("rightSpeed:", rightSpeed);
+                telemetry.addData("leftSpeed", leftSpeed);
+                telemetry.update();
+                updateMotors(rightSpeed, leftSpeed, throwerSpeed);
+
+
+            }
+            else{
+                updateMotors(rightSpeed,leftSpeed,throwerSpeed);
+            }
+            if(gamepad1.x){
+                if(throwerSpeed != 0) {
+                    throwerSpeed = 0;
+                }
+                else{
+                    throwerSpeed = 0.5F;
+                }
+                updateMotors(0,0,throwerSpeed);
+            }
+        }
+
+
+
+
+
+
+}
+
+    private void updateMotors(float rightSpeed, float leftSpeed, float throwerSpeed) {
+        rightFrontDrive.setPower(rightSpeed);
+        leftFrontDrive.setPower(leftSpeed);
+        thrower.setPower(throwerSpeed);
     }
-    else if (gamepad1.left_trigger > 0);
-        speed = -gamepad1.left_trigger;
-        telemetry.addData("Speed:", speed);
-        telemetry.update();
-        leftFrontDrive.setPower(speed);
-//     leftBackDrive.setPower(speed);
-        rightFrontDrive.setPower(speed);
-//     rightBackDrive.setPower(speed);
-}
 
-
-
-
-
-
-}
 
 }
