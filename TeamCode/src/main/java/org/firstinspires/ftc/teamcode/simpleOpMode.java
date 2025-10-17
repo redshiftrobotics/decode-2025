8836c48c7b9c1443fa5d27cb374864f39752213a
Parzivalrp2 <amithendrickson@seattleacademy.org>
2025-10-16 17:00:00 -0700

    patch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simpleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simpleOpMode.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "hunter2")
+@TeleOp(name = "hunter3")
 public class simpleOpMode extends LinearOpMode {
 
     DcMotor leftFrontDrive;
@@ -26,52 +26,55 @@ public class simpleOpMode extends LinearOpMode {
         leftFrontDrive = hardwareMap.get(DcMotor.class, "FL");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "FR");
         thrower = hardwareMap.get(DcMotor.class, "T");
+
 //        leftStopper = hardwareMap.get(Servo.class, "LS");
 //        rightStopper = hardwareMap.get(Servo.class, "RS");
+
         telemetry.addData("Status:", "Initialized");
         telemetry.update();
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        thrower.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         waitForStart();
+
         float throwerSpeed = 0;
         float rightSpeed = 0;
         float leftSpeed = 0;
         while (opModeIsActive()) {
 
-            if ((gamepad1.right_trigger > 0) || (gamepad1.left_trigger >0)) {
-                rightSpeed = gamepad1.right_trigger;
-                leftSpeed = gamepad1.left_trigger;
-                telemetry.addData("rightSpeed:", rightSpeed);
-                telemetry.addData("leftSpeed", leftSpeed);
-                telemetry.update();
-                updateMotors(rightSpeed, leftSpeed, throwerSpeed);
+            rightSpeed = (gamepad1.right_trigger*gamepad1.right_trigger) - (gamepad1.left_trigger*gamepad1.left_trigger);
+            leftSpeed = (gamepad1.right_trigger*gamepad1.right_trigger) - (gamepad1.left_trigger*gamepad1.left_trigger);
 
+            telemetry.addData("rightSpeed:", rightSpeed);
+            telemetry.addData("leftSpeed", leftSpeed);
+            telemetry.update();
 
-            }
-            else{
-                updateMotors(rightSpeed,leftSpeed,throwerSpeed);
-            }
-            if(gamepad1.x){
+            if(gamepad1.xWasPressed()){
                 if(throwerSpeed != 0) {
                     throwerSpeed = 0;
                 }
                 else{
                     throwerSpeed = 0.8F;
                 }
-                updateMotors(0,0,throwerSpeed);
+                
             }
+            //
+            if (leftSpeed < 0) {
+                leftFrontDrive.setPower(-MathUtils.clamp(leftSpeed, 0.02, 1));
+            }
+            else{
+                leftFrontDrive.setPower(-MathUtils.clamp(Math.abs(leftSpeed), 0.02, 1));
+            }
+
+            if (rightSpeed < 0) {
+                rightFrontDrive.setPower(MathUtils.clamp(rightSpeed, 0.02, 1));
+            }
+            else{
+                rightFrontDrive.setPower(MathUtils.clamp(Math.abs(rightSpeed), 0.02, 1));
+            }
+            thrower.setPower(throwerSpeed);
         }
-
-
-
-
-
-
-}
-
-    private void updateMotors(float rightSpeed, float leftSpeed, float throwerSpeed) {
-        rightFrontDrive.setPower(MathUtils.clamp(rightSpeed, 0.02, 1));
-        leftFrontDrive.setPower(MathUtils.clamp(leftSpeed, 0.02, 1));
-        thrower.setPower(throwerSpeed);
     }
-
-
 }
