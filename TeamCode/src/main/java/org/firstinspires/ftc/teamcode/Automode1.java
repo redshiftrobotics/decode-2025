c5798375fbf80aa181305d109c4db56390e74bad
elliottong15-arch <elliottong15@gmail.com>
2025-10-18 14:02:36 -0700

    Update Automode1.java
    Changed automode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Automode1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Automode1.java
@@ -45,82 +45,115 @@ public class Automode1 extends LinearOpMode {
         rightBackDrive.setpower(0.0);
     }
 
+        public static class MotorBob{
+            private string name;
+            private double power;
+
+            public Motor(string motorBob){
+                this.name = Bob;
+                this.power = 0.0;
+            }
+                public void setpower(double power){
+                    this.currentpower = power1;
+            }
+
+
+        public double getPower() {
+            return currentPower;
+        }
+    }
+    //Making an array for all of the motors
+     public static void main(String[] args) {
+    
+        Motor FL = new Motor("Front Left");
+        Motor FR = new Motor("Front Right");
+        Motor BL = new Motor("Back Left");
+        Motor BR = new Motor("Back Right");
+
+        FourWheelerAutonomous Robot = new FourWheelerAutonomous(FL, FR, BL, BR);
+
+        // Drive forward at 30% power for 5 seconds
+        Robot.driveForward(0.3, 2000);
+    }
+}
+
+
     
 
-    @Override
-     public void runOpMode() {
-     leftFrontDrive = hardwareMap.get(DcMotor.class, "FL");
-      rightFrontDrive = hardwareMap.get(DcMotor.class, "FR");
+   // @Override
+    // public void runOpMode() {
+    // leftFrontDrive = hardwareMap.get(DcMotor.class, "FL");
+    //  rightFrontDrive = hardwareMap.get(DcMotor.class, "FR");
 
 
-     telemetry.addData("Status", "Initialized").setRetained(true);
-       telemetry.update();
+     //telemetry.addData("Status", "Initialized").setRetained(true);
+       //telemetry.update();
 
-      waitForStart();
-   runtime.reset();
+      //waitForStart();
+   //runtime.reset();
 
-    if (opModeIsActive()) {
+    //if (opModeIsActive()) {
 
-            telemetry.addData("Status", "Run Time: %s", runtime.toString());
+            //telemetry.addData("Status", "Run Time: %s", runtime.toString());
 
-            New movement.
-           waitSeconds(10);
+            //New movement.
+           //waitSeconds(10);
 
-           driveForwardInches(20);
+           //driveForwardInches(20);
 
         }
     }
 
-    public static void waitSeconds(double seconds) {
-      try {
-            Thread.sleep(Math.round(seconds * 1000.0));
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-    }
+    //public static void waitSeconds(double seconds) {
+      //try {
+        //  Thread.sleep(Math.round(seconds * 1000.0));
+   //     } catch (InterruptedException e) {
+           // Thread.currentThread().interrupt();
+      //  }
+ //   }
 
 
-    public void driveForwardInches(double inchesToGo) {
+   // public void driveForwardInches(double inchesToGo) {
 
         // figure out what time it is.
-    ElapsedTime time1 = new ElapsedTime();
+  //  ElapsedTime time1 = new ElapsedTime();
 
 
         // turn on the motors.
-        leftFrontDrive.setPower(1);
-        rightFrontDrive.setPower(1);
+       // leftFrontDrive.setPower(1);
+        //rightFrontDrive.setPower(1);
 
         // when five seconds have elapsed, turn off motors.
-        double currentMillis = time1.milliseconds(); // Or a similar getter
+        //double currentMillis = time1.milliseconds(); // Or a similar getter
         //double newMillis = currentMillis + (5 * 1000L); // Add 5 seconds (5000 milliseconds)
         //ElapsedTime time2 = new ElapsedTime(newMillis); // Or a similar setter
 
-        while (currentMillis < 5000) {
+        //while (currentMillis < 5000) {
             // do nothing and wait till the 5 elapse
-            currentMillis = time1.milliseconds();
+          //  currentMillis = time1.milliseconds();
 
-            telemetry.addData("Status", "Initialized");
-            telemetry.update();
+            //telemetry.addData("Status", "Initialized");
+            //telemetry.update();
 
         }
-        telemetry.addData("Status", "Finished waiting");
-        telemetry.update();
+        //telemetry.addData("Status", "Finished waiting");
+       // telemetry.update();
 
-        leftFrontDrive.setPower(0);
-        rightFrontDrive.setPower(0);
-
-
-
-        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+       // leftFrontDrive.setPower(0);
+        //rightFrontDrive.setPower(0);
 
 
-        telemetry.clear();
+
+        //leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        //telemetry.clear();
     }
 
 
-    public double ticksToInch(int ticks) {
-        return (ticks * Constants.OdometryConstants.tickInMM) / 25.4;
+    //public double ticksToInch(int ticks) {
+        //return (ticks * Constants.OdometryConstants.tickInMM) / 25.4;
     }
 
 }
