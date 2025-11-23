d895bf490fdf4c4d4194b973066dea01c4a90b75
Parzivalrp2 <amithendrickson@seattleacademy.org>
2025-11-23 12:14:11 -0800

    increased TeleOp thrower power by 0.04

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -46,7 +46,7 @@ public class TeleOp extends LinearOpMode {
         float rightSpeed;
         float leftSpeed;
         boolean slowMode = false;
-        throwerSpeed = TeleOpConstants.THROWER_POWER;
+        throwerSpeed = TeleOpConstants.THROWER_POWER + 0.04F;
         leftLED.setPosition(0);
         rightLED.setPosition(0);
         while (opModeIsActive()) {
@@ -59,7 +59,7 @@ public class TeleOp extends LinearOpMode {
                 leftSpeed = leftSpeed + gamepad1.left_stick_x;
             }
             if (gamepad1.xWasPressed()){
-                throwerSpeed = TeleOpConstants.THROWER_POWER;
+                throwerSpeed = TeleOpConstants.THROWER_POWER + 0.04F;
             }
             if(gamepad1.bWasPressed()){
                 throwerSpeed = 0;
